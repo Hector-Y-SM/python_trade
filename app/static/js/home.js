b31160efc99ae3f1a2a96c5285f62b08703b9bae
Hector-Y-SM <hm96012@gmail.com>
2024-12-18 21:53:39 -0800

    first version of user to vendor conversion

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -1,1 +1,65 @@
 const btn_user_to_seller = document.getElementById('btn_user_to_seller');
+const btn_submit_seller = document.getElementById('btn_submit_seller');
+
+const form_modal = document.getElementById('form_modal');
+form_modal.style.display = 'none';
+const btn_close_modal = document.getElementById('btn_close_modal');
+
+btn_user_to_seller.addEventListener('click', async () => {
+    try{
+        const user_email = sessionStorage.getItem('user_email');
+        const inp_name = document.getElementById('seller_name');
+        const inp_email = document.getElementById('seller_email');
+        const inp_number = document.getElementById('seller_phone');
+        const inp_password = document.getElementById('seller_password');
+
+        const response = await fetch('/get_user_data',{
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({user_email}),  
+        });
+
+        if ( response.ok ){
+            const user_data = await response.json();
+            inp_name.value = user_data.name;
+            inp_email.value = user_data.email;
+            inp_number.value = user_data.number;
+
+            btn_submit_seller.addEventListener('click', () =>{
+                const data = {
+                    name: inp_name.value,
+                    email: inp_email.value,
+                    password: inp_password.value,
+                    cell_phone: inp_number.value
+                }
+
+                fetch('/user_to_seller', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data),
+                })
+                .then(response => response.json())  
+                .then(data => {
+                    console.log(data.message);  
+                    window.location.href = 'home_seller.html';
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+
+            })
+        } else {
+            const error = await response.json();
+            console.log('error ',error.message)
+        }
+
+    } catch(error){ console.log('error '. error) }
+    
+    form_modal.style.display = 'flex'; 
+});
+
+btn_close_modal.addEventListener('click', () => { form_modal.style.display = 'none'; });
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import create_app
 from flask import render_template, request, jsonify
 from app.models.User import User
+from app.models.Seller import Seller
 
 app = create_app()
 usuarios = []
@@ -48,7 +49,7 @@ def login_usr():
 @app.route('/get_user_data', methods=['POST'])
 def get_user_data():
     data = request.get_json()
-    email = data.get('email')
+    email = data.get('user_email')
     flag = False
     user = {}
 
@@ -61,12 +62,38 @@ def get_user_data():
         return jsonify({
             "name": user.name,
             "email": user.email,
-            "number": user.number,
-            "password": user.password
+            "number": user.number
+            #"password": user.password
         }), 200
     else:
         return jsonify({"message":"usuario no encontrado"}), 401
 
+@app.route('/user_to_seller', methods=['POST'])
+def user_to_seller():
+    #TODO
+    # cambiar el uso de next, por algo mas efectivo, un ciclo tal vez, (a veces falla next)
+    data = request.get_json()
+
+    email = data.get('email')
+    if not email:
+        return jsonify({"message":"el campo del email es obligatorio"}), 400
+    
+    user = next((x for x in usuarios if x.email == email), None)
+    if not user:
+        return jsonify({"message":"usuario no encontrado"}), 400
+    
+    name = data.get('name', user.name)
+    cell_phone = data.get('cell_phone', user.number)
+    password = data.get('password', user.password)
+
+    seller = Seller(name, email, cell_phone, password)
+    usuarios.remove(user)
+    vendedores.append(seller)
+
+
+    print(len(usuarios))
+    print(len(vendedores))
+    return jsonify({"message":"te has convertido en vendedor exitosamente"}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
